36ea90ebd26105c1c6db76123d70f379392b0136
Dave Kisly <target@uniserve.com>
2014-05-12 11:32:07 -0700

    removed the block ID in the faceted search

--- templates/block--facetapi-skiplinks.tpl.php
+++ templates/block--facetapi-skiplinks.tpl.php
@@ -6,7 +6,7 @@
 
 	<?php print render($title_prefix); ?>
 		<?php if ($title): ?>
-			<h2<?php print $title_attributes; ?>><?php print $title; ?> ... ID: <?php print $block_id; ?></h2>
+			<h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
 		<?php endif; ?>
 	<?php print render($title_suffix); ?>
 
