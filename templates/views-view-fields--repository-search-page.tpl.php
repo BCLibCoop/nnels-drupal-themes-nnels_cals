52b1dd449f92b74bedf039f20a03ab9e97d1192d
Jonathan Schatz <jonathan.schatz@bc.libraries.coop>
2018-07-11 19:47:27 -0700

    In browse and search result displays, hide the File Resource embed's empty message when there are external (Resource) URL values available
    Signed-off-by: Jonathan Schatz <jonathan.schatz@bc.libraries.coop>

---
+++ templates/views-view-fields--repository-search-page.tpl.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to all the fields as a row.
+ *
+ * - $view: The view in use.
+ * - $fields: an array of $field objects. Each one contains:
+ *   - $field->content: The output of the field.
+ *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
+ *   - $field->class: The safe class id to use.
+ *   - $field->handler: The Views field handler object controlling this field. Do not use
+ *     var_export to dump this object, as it can't handle the recursion.
+ *   - $field->inline: Whether or not the field should be inline.
+ *   - $field->inline_html: either div or span based on the above flag.
+ *   - $field->wrapper_prefix: A complete wrapper containing the inline_html to use.
+ *   - $field->wrapper_suffix: The closing tag for the wrapper.
+ *   - $field->separator: an optional separator that may appear before a field.
+ *   - $field->label: The wrap label text to use.
+ *   - $field->label_html: The full HTML of the label to use including
+ *     configured element type.
+ * - $row: The raw result object from the query, with all data it fetched.
+ *
+ * @ingroup views_templates
+ */
+?>
+
+<?php
+// Some Repository Items only use the external URL field ("Resource URL")
+// in place of File Resources. If we find an item using field_urls_external
+// and having no File Resource field collections, we set to null whatever the
+// File Resource field has cooked up.
+if ( empty ( $row->_entity_properties['entity object']->field_file_resource )
+  && ! empty($fields['field_urls_external']->content) ) {
+  $fields['view'] = NULL;
+}
+?>
+
+<?php foreach ($fields as $id => $field): ?>
+  <?php if (!empty($field->separator)): ?>
+    <?php print $field->separator; ?>
+  <?php endif; ?>
+
+  <?php print $field->wrapper_prefix; ?>
+    <?php print $field->label_html; ?>
+    <?php print $field->content; ?>
+  <?php print $field->wrapper_suffix; ?>
+<?php endforeach; ?>
